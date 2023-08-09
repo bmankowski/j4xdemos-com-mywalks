bfdd53787b60827b8d482ab49d6489c64912f188
bmankowski <bmankowski@gmail.com>
2023-08-09 12:42:16 +0200

    Dodanie position_name

--- com_jobpositions/admin/src/Model/JobpositionsModel.php
+++ com_jobpositions/admin/src/Model/JobpositionsModel.php
@@ -36,7 +36,7 @@ class JobpositionsModel extends ListModel
 			$config['filter_fields'] = array(
 				'id', 'a.id',
 				'title', 'a.title',
-				'job_position', 'a.job_position',
+				'position_name', 'a.position_name',
 				'description', 'a.description',
 				'distance', 'a.distance',
 				'toilets', 'a.toilets',
