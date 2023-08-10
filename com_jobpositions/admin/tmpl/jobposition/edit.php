a1502b9820ec9a25af1e24894a86e70856c79c77
bmankowski <bmankowski@gmail.com>
2023-08-10 14:24:29 +0200

    Dodanie nowej zakładki do edycji.

--- com_jobpositions/admin/tmpl/jobposition/edit.php
+++ com_jobpositions/admin/tmpl/jobposition/edit.php
@@ -6,7 +6,6 @@
  * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die;
 
 use Joomla\CMS\HTML\HTMLHelper;
@@ -16,66 +15,73 @@ use Joomla\CMS\Router\Route;
 
 HTMLHelper::_('behavior.formvalidator');
 HTMLHelper::_('behavior.keepalive');
-
 ?>
 
 <form action="<?php echo Route::_('index.php?option=com_jobpositions&view=jobposition&layout=edit&id=' . (int) $this->item->id); ?>"
-	method="post" name="adminForm" id="jobpositions-form" class="form-validate">
+      method="post" name="adminForm" id="jobpositions-form" class="form-validate">
 
-	<?php echo LayoutHelper::render('joomla.edit.title_alias', $this); ?>
+    <?php echo LayoutHelper::render('joomla.edit.title_alias', $this); ?>
 
-	<div>
-		<?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', array('active' => 'details')); ?>
+    <div>
+        <?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', array('active' => 'details')); ?>
 
-		<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'details', Text::_('COM_JOBPOSITIONS_JOBPOSITION_TAB_DETAILS')); ?>
-		<div class="row">
-			<div class="col-md-9">
-				<div class="row">
-					<div class="col-md-6">
-						<?php echo $this->form->renderField('description'); ?>
-						<?php echo $this->form->renderField('job_position_name'); ?>
-						<?php echo $this->form->renderField('job_description'); ?>
-						<?php echo $this->form->renderField('job_location'); ?>
-						<?php echo $this->form->renderField('job_start_date'); ?>
-						<?php echo $this->form->renderField('job_end_date'); ?>
-						<?php echo $this->form->renderField('distance'); ?>
-						<?php echo $this->form->renderField('id'); ?>
-					</div>
-				</div>
-			</div>
-			<div class="col-md-3">
-				<div class="card card-light">
-					<div class="card-body">
-						<?php echo LayoutHelper::render('joomla.edit.global', $this); ?>
-					</div>
-				</div>
-			</div>
-		</div>
-		<?php echo HTMLHelper::_('uitab.endTab'); ?>
+        <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'details', Text::_('COM_JOBPOSITIONS_JOBPOSITION_TAB_DETAILS')); ?>
+        <div class="row">
+            <div class="col-md-9">
+                <div class="row">
+                    <div class="col-md-6">
+                        <?php echo $this->form->renderField('description'); ?>
+                        <?php echo $this->form->renderField('job_position_name'); ?>
+                        <?php echo $this->form->renderField('distance'); ?>
+                        <?php echo $this->form->renderField('id'); ?>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card card-light">
+                    <div class="card-body">
+                        <?php echo LayoutHelper::render('joomla.edit.global', $this); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php echo HTMLHelper::_('uitab.endTab'); ?>
 
-		<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'options', Text::_('COM_JOBPOSITIONS_JOBPOSITION_TAB_OPTIONS')); ?>
-		<div class="row">
-			<div class="col-md-12">
-				<?php echo $this->form->renderField('toilets'); ?>
-				<?php echo $this->form->renderField('cafe'); ?>
-				<?php echo $this->form->renderField('bogs'); ?>
-			</div>
-		</div>
-		<?php echo HTMLHelper::_('uitab.endTab'); ?>
+        <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'options', Text::_('COM_JOBPOSITIONS_JOBPOSITION_TAB_JOBDETAILS')); ?>
+        <div class="row">
+            <div class="col-md-12">
+                <?php echo $this->form->renderField('job_description'); ?>
+                <?php echo $this->form->renderField('job_location'); ?>
+                <?php echo $this->form->renderField('job_start_date'); ?>
+                <?php echo $this->form->renderField('job_end_date'); ?> 
+            </div>
+        </div>
+        <?php echo HTMLHelper::_('uitab.endTab'); ?>            
 
-		<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'picture', Text::_('COM_JOBPOSITIONS_JOBPOSITION_TAB_PICTURE')); ?>
-		<div class="row">
-			<div class="col-md-12">
-				<?php echo $this->form->renderField('picture'); ?>
-				<?php echo $this->form->renderField('width'); ?>
-				<?php echo $this->form->renderField('height'); ?>
-				<?php echo $this->form->renderField('alt'); ?>
-			</div>
-		</div>
-		<?php echo HTMLHelper::_('uitab.endTab'); ?>
 
-		<?php echo HTMLHelper::_('uitab.endTabSet'); ?>
-	</div>
-	<input type="hidden" name="task" value="">
-	<?php echo HTMLHelper::_('form.token'); ?>
+        <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'options', Text::_('COM_JOBPOSITIONS_JOBPOSITION_TAB_OPTIONS')); ?>
+        <div class="row">
+            <div class="col-md-12">
+                <?php echo $this->form->renderField('toilets'); ?>
+                <?php echo $this->form->renderField('cafe'); ?>
+                <?php echo $this->form->renderField('bogs'); ?>
+            </div>
+        </div>
+        <?php echo HTMLHelper::_('uitab.endTab'); ?>
+
+        <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'picture', Text::_('COM_JOBPOSITIONS_JOBPOSITION_TAB_PICTURE')); ?>
+        <div class="row">
+            <div class="col-md-12">
+                <?php echo $this->form->renderField('picture'); ?>
+                <?php echo $this->form->renderField('width'); ?>
+                <?php echo $this->form->renderField('height'); ?>
+                <?php echo $this->form->renderField('alt'); ?>
+            </div>
+        </div>
+        <?php echo HTMLHelper::_('uitab.endTab'); ?>
+
+        <?php echo HTMLHelper::_('uitab.endTabSet'); ?>
+    </div>
+    <input type="hidden" name="task" value="">
+    <?php echo HTMLHelper::_('form.token'); ?>
 </form>
