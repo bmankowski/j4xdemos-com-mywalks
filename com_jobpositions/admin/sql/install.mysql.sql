295b14e9e6d27bc085c538409f98a976e5617428
bmankowski <bmankowski@gmail.com>
2023-08-16 18:51:48 +0200

    Dodane nowe pola do bazy danych i więcej rekordów wstępnych

--- com_jobpositions/admin/sql/install.mysql.sql
+++ com_jobpositions/admin/sql/install.mysql.sql
@@ -5,11 +5,30 @@
 
 CREATE TABLE IF NOT EXISTS `#__jobpositions` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `job_position_name` text NOT NULL,
+  `job_position_name` varchar(128) NOT NULL,
+  `job_description` text NOT NULL,
   `job_location` text NOT NULL,
   `job_start_date` date NOT NULL,
   `job_end_date` date NOT NULL,
-  `job_description` text NOT NULL,
+  `job_end_date` date NOT NULL,
+  `job_contract_type` text NOT NULL,
+  `job_part_of_employment` text NOT NULL,
+  `job_part_of_employment` text NOT NULL, --part-time, full-time
+  `job_level` text NOT NULL,
+  `job_employment_type` text NOT NULL, --hybrid, on-site
+  `job_recruitment_type` text NOT NULL,
+  `job_technologies_required` text NOT NULL,
+  `job_technologies_nice_to_have` text NOT NULL,
+  `job_operating systems` text NOT NULL,
+  `job_duties` text NOT NULL,
+  `job_requirements` text NOT NULL,
+  `job_nice_to_have` text NOT NULL,
+  `job_project_management_system` text NOT NULL,
+  `job_what_to_offer` text NOT NULL,
+  `job_ssportcardportcard` tinyint(1) NOT NULL DEFAULT '0',
+  `job_healthplan` tinyint(1) NOT NULL DEFAULT '0',
+  `job_healthplan` tinyint(1) NOT NULL DEFAULT '0',
+  `job_lifeinsurance` tinyint(1) NOT NULL DEFAULT '0',
   `description` text NOT NULL,
   `distance` decimal(10,0) NOT NULL,
   `toilets` tinyint(1) NOT NULL DEFAULT '0',
@@ -39,6 +58,24 @@ INSERT IGNORE INTO `#__jobpositions` (`id`, `job_position_name` ,`job_descriptio
 (9, 'Analityk HTML', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Siedlce','2023-02-6','', 'Walk around the lake on an accessible path.', '2', 1, 1, 0, 0, NULL, NULL, NULL, ''),
 (10,'Analityk Java', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Siedlce','2023-02-6','', 'Walk around the lake on an accessible path.', '2', 1, 1, 0, 0, NULL, NULL, NULL, ''),
 (11,'Programista Python / Django', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Wrocław','2023-03-7','', 'Walk along the line of the old railway track from start point car park to Thing castle', '2', 1, 1, 0, 0, NULL, NULL, NULL, '');
+(12, 'Programista SAS', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Kraków','2023-01-11','', 'Woodland walk on hard paths', '4', 0, 0, 1, 1, NULL, NULL, NULL, ''),
+(13, 'Programista Senior Vue', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Gdańsk','2023-01-15','', 'Hill walk with good views on established path.', '6', 0, 0, 3, 2, NULL, NULL, NULL, ''),
+(14, 'Programista Java', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Siedlce','2023-02-6','', 'Walk around the lake on an accessible path.', '2', 1, 1, 0, 0, NULL, NULL, NULL, ''),
+(15, 'Programista HTML', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Siedlce','2023-02-6','', 'Walk around the lake on an accessible path.', '2', 1, 1, 0, 0, NULL, NULL, NULL, ''),
+(16, 'Analityk CSS', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Siedlce','2023-02-6','', 'Walk around the lake on an accessible path.', '2', 1, 1, 0, 0, NULL, NULL, NULL, ''),
+(17, 'Analityk Java', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Siedlce','2023-02-6','', 'Walk around the lake on an accessible path.', '2', 1, 1, 0, 0, NULL, NULL, NULL, ''),
+(18, 'Analityk CSS', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Siedlce','2023-02-6','', 'Walk around the lake on an accessible path.', '2', 1, 1, 0, 0, NULL, NULL, NULL, ''),
+(19, 'Analityk HTML', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Siedlce','2023-02-6','', 'Walk around the lake on an accessible path.', '2', 1, 1, 0, 0, NULL, NULL, NULL, ''),
+(20,'Analityk Java', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Siedlce','2023-02-6','', 'Walk around the lake on an accessible path.', '2', 1, 1, 0, 0, NULL, NULL, NULL, ''),
+(21,'Programista Python / Django', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Wrocław','2023-03-7','', 'Walk along the line of the old railway track from start point car park to Thing castle', '2', 1, 1, 0, 0, NULL, NULL, NULL, '');
+(22, 'Programista SAS', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Kraków','2023-01-11','', 'Woodland walk on hard paths', '4', 0, 0, 1, 1, NULL, NULL, NULL, ''),
+(23, 'Programista Senior Vue', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Gdańsk','2023-01-15','', 'Hill walk with good views on established path.', '6', 0, 0, 3, 2, NULL, NULL, NULL, ''),
+(24, 'Programista Java', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Siedlce','2023-02-6','', 'Walk around the lake on an accessible path.', '2', 1, 1, 0, 0, NULL, NULL, NULL, ''),
+(25, 'Programista HTML', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Siedlce','2023-02-6','', 'Walk around the lake on an accessible path.', '2', 1, 1, 0, 0, NULL, NULL, NULL, ''),
+(26, 'Analityk CSS', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Siedlce','2023-02-6','', 'Walk around the lake on an accessible path.', '2', 1, 1, 0, 0, NULL, NULL, NULL, ''),
+(27, 'Analityk Java', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Siedlce','2023-02-6','', 'Walk around the lake on an accessible path.', '2', 1, 1, 0, 0, NULL, NULL, NULL, ''),
+(28, 'Analityk CSS', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Siedlce','2023-02-6','', 'Walk around the lake on an accessible path.', '2', 1, 1, 0, 0, NULL, NULL, NULL, ''),
+(29, 'Analityk HTML', '<h1>Kogo szukamy</h1>Prawdopodobnie Ciebie!','Siedlce','2023-02-6','', 'Walk around the lake on an accessible path.', '2', 1, 1, 0, 0, NULL, NULL, NULL, ''),
 
 -- --------------------------------------------------------
 
