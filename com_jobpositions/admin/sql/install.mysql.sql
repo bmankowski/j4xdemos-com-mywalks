f1685e10fa96fa63bf92cb2e39f4adc29390a14e
bmankowski <bmankowski@gmail.com>
2023-08-18 18:06:59 +0200

    Część HTML jest przepisana na nowego CSS

--- com_jobpositions/admin/sql/install.mysql.sql
+++ com_jobpositions/admin/sql/install.mysql.sql
@@ -44,15 +44,16 @@ CREATE TABLE IF NOT EXISTS #__jobpositions (
 
 INSERT INTO #__jobpositions (job_position_name, job_remuneration_from, job_remuneration_to, job_remuneration_unit, job_description, job_location, job_start_date, job_end_date, job_contract_type, 
 job_part_of_employment, job_level, job_employment_type,job_recruitment_type,job_technologies_required, job_technologies_optional, job_operating_systems, job_duties, 
-job_requirements,job_requirements_optional,job_project_management_system,
-job_what_to_offer,
-job_sportcard,job_healthplan,job_lifeinsurance) VALUES 
-("Analityk SQL", "12000", "15000", "netto (+ VAT) mies.", "Praca w stabilnej firmie, która od 12 lat stale obsługuje T-Mobile i Orange, a obecnie także szybko rozwija świadczenie usług dla innych klientów na rynku krajowym i międzynarodowym", 
-"Warszawa", "2023-01-01", "2024-06-01", "umowa zlecenie, kontrakt B2B","Cały etat","specjalista (Mid / Regular)","praca hybrydowa","rekrutacja zdalna","<ul><li>SQL</li>NOSQL<li></li><li>Konteneryzacja</li></ul>","<ul><li>ORACLE</li>Git<li></li><li>Java</li></ul>","<ul><li>Linux</li></ul>","<ul><li>Administracja i utrzymanie systemów OSS </li><li>Instalacja i utrzymanie środowisk opartych o systemy: Linux, bazy danych typu Sql i NoSql</li></ul>",
-"<ul><li>SQL</li><li></li><li></li></ul>","<ul><li>SQL</li><li></li><li></li></ul>","<ul><li>Doświadczenie w administracji relacyjnych baz danych jak Oracle, MySQL</li></ul>",
+job_requirements,job_requirements_optional,job_project_management_system,job_what_to_offer,job_sportcard,job_healthplan,job_lifeinsurance) VALUES 
+("Analityk SAS", "12000", "15000", "netto (+ VAT) mies.", "Praca w stabilnej firmie, która od 12 lat stale obsługuje T-Mobile i Orange, a obecnie także szybko rozwija świadczenie usług dla innych klientów na rynku krajowym i międzynarodowym", 
+"Warszawa", "2023-01-01", "2024-06-01", "umowa zlecenie, kontrakt B2B","Cały etat","specjalista (Mid / Regular)","praca hybrydowa","rekrutacja zdalna","<ul><li>SQL</li><li>NOSQL</li><li>Konteneryzacja</li></ul>","<ul><li>ORACLE</li><li>Git</li><li>Java</li></ul>","<ul><li>Linux</li></ul>","<ul><li>Administracja i utrzymanie systemów OSS </li><li>Instalacja i utrzymanie środowisk opartych o systemy: Linux, bazy danych typu Sql i NoSql</li></ul>",
+"<ul><li>SQL</li><li>SAS</li><li>Oracle</li></ul>","<ul><li>SQL</li><li></li><li></li></ul>","<ul><li>Doświadczenie w administracji relacyjnych baz danych jak Oracle, MySQL</li></ul>",
 "<ul><li>SQL</li>Dołączenie do doświadczonego zespołu profesjonalistów, którzy chętnie dzielą się swoją wiedzą i umiejętnościami<li></li><li>Dostęp do najnowszych technologii i rozbudowanego środowiska IT</li></ul>",
 true, true, false)
 ;
+
+
+
 
 
 -- --------------------------------------------------------
